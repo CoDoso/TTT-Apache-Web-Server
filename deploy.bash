f6790c82dc747ee8e6438907c5e05d2559c54c1c
CoDoso <155891549+CoDoso@users.noreply.github.com>
2024-06-24 08:36:24 +0200

    Deploy File

---
+++ deploy.bash
@@ -0,0 +1,5 @@
+sudo chown -R www-data:www-data /var/www/flaskapp
+
+sudo mkdir -p /var/www/flaskapp
+sudo cp /path/to/your/app.py /var/www/flaskapp/
+sudo cp /path/to/your/flaskapp.wsgi /var/www/flaskapp/
